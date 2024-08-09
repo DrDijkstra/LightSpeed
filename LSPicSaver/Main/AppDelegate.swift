a2a1fea11a50efb4bf181e492b4446a9d254d090
Sanjay Dey <deysanjay121@gmail.com>
2024-08-10 02:25:44 +0600

    added LightspeedCore for managing core business logic and API interactions.

--- LSPicSaver/Main/AppDelegate.swift
+++ LSPicSaver/Main/AppDelegate.swift
@@ -8,7 +8,6 @@
 import UIKit
 import CoreData
 
-
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
--- LightSpeedCore/Sources/LightSpeedCore/LightSpeedCore.swift
+++ LightSpeedCore/Sources/LightSpeedCore/LightSpeedCore.swift
@@ -1,7 +1,24 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
+//
+//  MainApplication.swift
+//  LSPicSaver
+//
+//  Created by Sanjay Dey on 10/8/24.
+//
 
 public class LightSpeedCore {
     
+    static var selfRef : LightSpeedCore? = nil
+
+    public static func getInstance() -> LightSpeedCore {
+        guard selfRef != nil else {
+            selfRef = LightSpeedCore()
+            return selfRef!
+            
+        }
+        return selfRef!
+    }
+    
+    private init () {
+        
+    }
 }
