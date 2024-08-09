82980c8bf58625720e51ca0446f824c199a7e30f
Sanjay Dey <deysanjay121@gmail.com>
2024-08-10 01:53:49 +0600

    Enhanced modularity and maintainability of app initialization code

---
+++ LSPicSaver/Main/MainApplication.swift
@@ -0,0 +1,13 @@
+//
+//  MainApplication.swift
+//  LSPicSaver
+//
+//  Created by Sanjay Dey on 10/8/24.
+//
+
+import Foundation
+import UIKit
+
+class MainApplication: UIApplication {
+    
+}
---
+++ LSPicSaver/Main/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  LSPicSaver
+//
+//  Created by Sanjay Dey on 10/8/24.
+//
+
+import UIKit
+
+UIApplicationMain(
+CommandLine.argc,
+CommandLine.unsafeArgv
+, NSStringFromClass(MainApplication.self), NSStringFromClass(AppDelegate.self))
