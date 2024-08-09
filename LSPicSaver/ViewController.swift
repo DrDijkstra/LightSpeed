05d39d5ce7107666d77e7c294468996cf4bacaa2
Sanjay Dey <deysanjay121@gmail.com>
2024-08-10 01:19:17 +0600

    created initial project

---
+++ LSPicSaver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSPicSaver
+//
+//  Created by Sanjay Dey on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
